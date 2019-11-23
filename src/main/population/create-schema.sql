96f5b9275028b86a3a0053ab420e853167bdc8fb
Miguel <Miguel@LAPTOP-II3I294M>
2019-11-23 21:38:02 +0100

    Initial commit

---
+++ src/main/population/create-schema.sql
@@ -0,0 +1,198 @@
+
+    create table `administrator` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `announcement` (
+       `id` integer not null,
+        `version` integer not null,
+        `moment` datetime(6),
+        `more_info` varchar(255),
+        `text` varchar(255),
+        `title` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `anonymous` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `authenticated` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `challenge` (
+       `id` integer not null,
+        `version` integer not null,
+        `deadline` datetime(6),
+        `description` varchar(255),
+        `reward_goal_bronze_amount` double precision,
+        `reward_goal_bronze_currency` varchar(255),
+        `reward_goal_gold_amount` double precision,
+        `reward_goal_gold_currency` varchar(255),
+        `reward_goal_silver_amount` double precision,
+        `reward_goal_silver_currency` varchar(255),
+        `title` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `company_record` (
+       `id` integer not null,
+        `version` integer not null,
+        `ceo` varchar(255),
+        `description` varchar(255),
+        `email` varchar(255),
+        `incorporated` bit not null,
+        `name` varchar(255),
+        `sector` varchar(255),
+        `telephone` varchar(255),
+        `website` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `consumer` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        `company` varchar(255),
+        `sector` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `curriculum` (
+       `id` integer not null,
+        `version` integer not null,
+        `moment` datetime(6),
+        `name` varchar(255),
+        `skills` varchar(255),
+        `studies` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `customisation` (
+       `id` integer not null,
+        `version` integer not null,
+        `customisations_en` varchar(255),
+        `customisations_es` varchar(255),
+        `threshold` double precision,
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `investor` (
+       `id` integer not null,
+        `version` integer not null,
+        `name` varchar(255),
+        `sector` varchar(255),
+        `stars` integer,
+        `statement` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `offer` (
+       `id` integer not null,
+        `version` integer not null,
+        `deadline` datetime(6),
+        `max_amount` double precision,
+        `max_currency` varchar(255),
+        `min_amount` double precision,
+        `min_currency` varchar(255),
+        `moment` datetime(6),
+        `text` varchar(255),
+        `ticker` varchar(255),
+        `title` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `provider` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        `company` varchar(255),
+        `sector` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `request` (
+       `id` integer not null,
+        `version` integer not null,
+        `deadline` datetime(6),
+        `description` varchar(255),
+        `moment` datetime(6),
+        `reward_amount` double precision,
+        `reward_currency` varchar(255),
+        `stars` integer,
+        `telephone` varchar(255),
+        `ticker` varchar(255),
+        `title` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `shout` (
+       `id` integer not null,
+        `version` integer not null,
+        `author` varchar(255),
+        `moment` datetime(6),
+        `text` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `user_account` (
+       `id` integer not null,
+        `version` integer not null,
+        `enabled` bit not null,
+        `identity_email` varchar(255),
+        `identity_name` varchar(255),
+        `identity_surname` varchar(255),
+        `password` varchar(255),
+        `username` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `hibernate_sequence` (
+       `next_val` bigint
+    ) engine=InnoDB;
+
+    insert into `hibernate_sequence` values ( 1 );
+
+    alter table `offer` 
+       add constraint UK_iex7e8fs0fh89yxpcnm1orjkm unique (`ticker`);
+
+    alter table `request` 
+       add constraint UK_9mxq3powq8tqctclj0fbi2nih unique (`ticker`);
+
+    alter table `user_account` 
+       add constraint UK_castjbvpeeus0r8lbpehiu0e4 unique (`username`);
+
+    alter table `administrator` 
+       add constraint FK_2a5vcjo3stlfcwadosjfq49l1 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
+
+    alter table `anonymous` 
+       add constraint FK_6lnbc6fo3om54vugoh8icg78m 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
+
+    alter table `authenticated` 
+       add constraint FK_h52w0f3wjoi68b63wv9vwon57 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
+
+    alter table `consumer` 
+       add constraint FK_6cyha9f1wpj0dpbxrrjddrqed 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
+
+    alter table `provider` 
+       add constraint FK_b1gwnjqm6ggy9yuiqm0o4rlmd 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
